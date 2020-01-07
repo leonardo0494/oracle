1ff3457240fdb2c52f64752e055a34dc493204b2
leonardo0494 <leonardosilvadev@gmail.com>
2020-01-07 19:11:05 -0300

    Adicionado projeto de auditoria da fila de processamento Siebel

---
+++ AUDIT_S_ESCL_REQ/s_escl_req_trigger_sbl8.sql
@@ -0,0 +1,48 @@
+DROP TABLE s_escl_req_audit_tbl;
+
+CREATE TABLE s_escl_req_audit_tbl (
+    CREATED date, 
+    CREATED_BY varchar2(15),
+    BT_ROW_ID varchar2(15),
+    GROUP_ID varchar2(15),
+    RULE_ID varchar2(15),
+    TBL_NAME varchar2(30),
+    GROUP_NAME varchar2(30),
+    RULE_NAME varchar2(100)
+);
+
+DROP TRIGGER siebel.s_escl_req_audit;
+
+CREATE OR REPLACE TRIGGER siebel.s_escl_req_audit
+AFTER INSERT ON siebel.s_escl_req
+FOR EACH ROW
+DECLARE
+    vGROUP varchar2(30);
+    vRULE varchar2(100);
+BEGIN
+
+    -- SELECT NOME GRUPO
+    SELECT 
+        NAME INTO vGROUP 
+    FROM 
+        SIEBEL.s_escl_group 
+    WHERE 
+        row_id = :NEW.group_id;
+    -- SELECT NOME REGRA
+    SELECT 
+        NAME INTO vRULE 
+    FROM 
+        SIEBEL.s_escl_rule 
+    WHERE 
+        row_id = :NEW.rule_id;
+
+    -- INSERINDO NA TABELA DE AUDITORIA
+
+    IF INSERTING THEN
+
+        INSERT INTO s_escl_req_audit_tbl (CREATED, CREATED_BY, BT_ROW_ID, GROUP_ID, RULE_ID, TBL_NAME, GROUP_NAME, RULE_NAME) 
+        VALUES(:NEW.created, :NEW.created_by, :NEW.bt_row_id, :NEW.group_id, :NEW.rule_id, :NEW.tbl_name, vGROUP, vRULE);
+
+    END IF;
+END;
+/
